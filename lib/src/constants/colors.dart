3adb1ac0e7c719cc8c1c511dad1236d489b53103
Barba2k2 <lorenzo.dz@hotmail.com>
2023-11-02 20:33:39 -0300

    added colors constants

---
+++ lib/src/constants/colors.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+//* -- DEFAULT COLORS
+const whiteColor = Colors.white;
+const white60 = Colors.white60;
+const blackColor = Colors.black;
+var greyShade600 = Colors.grey.shade600;
+
+//* -- BUTTONS COLORS
+const tPrimaryColor = Color(0xFFE3A223);
+const tSecondaryColor = Color(0xFFE37423);
+const registerTextButtonColor = Color(0xFF0BEF82);
+
+const tDarkCard = Color.fromARGB(255, 74, 74, 74);
+
+//* -- BOTTOM NAVIGATION BAR
+const darkNavBar = Color(0xFF2F2F2F);
+const whiteNavBar = Color(0xFFD8D8D8);
+const whiteBgNavBar = Color(0xFFF3F3F3);
+
+//* -- SNACKBAR COLORS
+const tSuccessSnackbar = Colors.green;
+const tWarningSnackbar = Colors.orange;
+const tErrorSnackbar = Colors.red;
+
+//* -- GENERAL COLROS
+const tWhiteColor = whiteColor;
+const tDarkColor = Color(0xff272727);
+const tCardBgColor = Color(0xFFF7F5F1);
+var subtitleColor = const Color.fromARGB(255, 220, 220, 220);
+
+//* -- SOCIAL Button COLORS
+const tGoogleBgColor = Color(0xFFDFEFFF);
+const tGoogleForegroundColor = Color(0xFF167EE6);
+const tFacebookBgColor = Color(0xFF1877F2);
+
+//* -- INPUT TEXT FIELD COLOR
+const darkInputText = Color.fromARGB(221, 72, 72, 72);
+const whiteInputText = whiteColor;
+
+//* -- REPORT DETAIL SCREEN
+const whiteContainer = Color(0xFFE0E0E0);
+const blackContainer = Color(0xFF424242);
