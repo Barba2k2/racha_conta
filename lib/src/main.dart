ec5b715e3d0abff9b84c183a93c81b2d3d6afdb5
Barba2k2 <lorenzo.dz@hotmail.com>
2023-11-06 11:38:42 -0300

    added firebase initialize app

--- lib/src/main.dart
+++ lib/src/main.dart
@@ -1,18 +1,35 @@
 import 'dart:developer';
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_config/flutter_config.dart';
+import 'package:get/get.dart';
 import 'package:racha_conta/src/app.dart';
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();  
+import 'firebase_options.dart';
+import 'repository/authentication_repository/authentication_repository.dart';
 
+Future<void> main() async {
+  // Garante a inicialização do binding de widgets do Flutter
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Inicializa o Firebase com as opções padrão para a plataforma atual e coloca o `AuthenticationRepository` no GetX para gerenciamento de estado
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform)
+      .then(
+    (_) => Get.put(
+      AuthenticationRepository(),
+    ),
+  );
+
+  // Carrega variáveis de ambiente usando FlutterConfig
   await FlutterConfig.loadEnvVariables();
 
+  // Configura um manipulador global de erros para registrar exceções no Flutter
   FlutterError.onError = (FlutterErrorDetails details) {
     log(details.exception.toString());
     log(details.stack.toString());
   };
 
+  // Inicia o aplicativo
   runApp(MyApp());
 }
