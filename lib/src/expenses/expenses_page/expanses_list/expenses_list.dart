d4eaff0dbedaee439b6d496f9cacf061150810aa
Barba2k2 <lorenzo.dz@hotmail.com>
2023-11-07 16:00:31 -0300

    added confirm dialog on delete expense

--- lib/src/expenses/expenses_page/expanses_list/expenses_list.dart
+++ lib/src/expenses/expenses_page/expanses_list/expenses_list.dart
@@ -19,10 +19,74 @@ class ExpenseList extends StatelessWidget {
       itemCount: expenses.length,
       itemBuilder: (ctx, index) => Dismissible(
         key: ValueKey(expenses[index]),
+        direction: DismissDirection.endToStart,
         background: Container(
-          color: Theme.of(context).colorScheme.error.withOpacity(.75),
-          margin: const EdgeInsets.symmetric(horizontal: 10),
+          decoration: BoxDecoration(
+            color: Theme.of(context).colorScheme.error.withOpacity(.75),
+            borderRadius: BorderRadius.circular(20),
+          ),
+          margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+          alignment: Alignment.centerRight,
+          child: const Padding(
+            padding: EdgeInsets.only(right: 20.0),
+            child: Icon(
+              Icons.delete,
+              color: Colors.white,
+              size: 28,
+            ),
+          ),
         ),
+        confirmDismiss: (direction) async {
+          return await showDialog<bool>(
+            context: context,
+            builder: (BuildContext dialogContext) {
+              return AlertDialog(
+                title: Text(
+                  'Confirmação',
+                  style: Theme.of(context).textTheme.headlineMedium,
+                  textAlign: TextAlign.center,
+                ),
+                content: Text(
+                  'Tem certeza que deseja apagar esta despesa?',
+                  style: Theme.of(context).textTheme.headlineSmall,
+                ),
+                actions: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      OutlinedButton(
+                        onPressed: () => Navigator.of(dialogContext).pop(false),
+                        style: OutlinedButton.styleFrom(
+                          padding: const EdgeInsets.symmetric(
+                            vertical: 15,
+                            horizontal: 40,
+                          ),
+                        ),
+                        child: const Text(
+                          'Não',
+                          style: TextStyle(fontSize: 16),
+                        ),
+                      ),
+                      ElevatedButton(
+                        style: OutlinedButton.styleFrom(
+                          padding: const EdgeInsets.symmetric(
+                            vertical: 15,
+                            horizontal: 40,
+                          ),
+                        ),
+                        child: const Text(
+                          'Sim',
+                          style: TextStyle(fontSize: 16),
+                        ),
+                        onPressed: () => Navigator.of(dialogContext).pop(true),
+                      ),
+                    ],
+                  ),
+                ],
+              );
+            },
+          );
+        },
         onDismissed: (direction) {
           onRemoveExpense(expenses[index]);
         },
