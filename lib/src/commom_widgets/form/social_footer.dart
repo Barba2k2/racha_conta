a1863195c9b896f4fd73eca0a26cab816f917cd9
Barba2k2 <lorenzo.dz@hotmail.com>
2023-11-13 16:04:19 -0300

    adjusts on social footer

--- lib/src/commom_widgets/form/social_footer.dart
+++ lib/src/commom_widgets/form/social_footer.dart
@@ -1,42 +1,66 @@
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import 'package:get/get.dart';
-import 'package:racha_conta/src/commom_widgets/buttons/social_button.dart';
-import 'package:racha_conta/src/constants/colors.dart';
 
-import '../../constants/image_strings.dart';
-import '../../constants/text_strings.dart';
-import '../../features/authentication/controllers/login_controller.dart';
-import '../buttons/clickable_richtext_widget.dart';
+import '../../Constants/colors.dart';
+import '../../Constants/image_strings.dart';
+import '../../Constants/text_strings.dart';
+import '../../Features/Authentication/Controllers/login_controller.dart';
+import '../Buttons/clickable_richtext_widget.dart';
+import '../Buttons/social_button.dart';
 
+// Widget que representa o rodapé contendo os botões de login social e um texto clicável.
 class SocialFooter extends StatelessWidget {
-  const SocialFooter(
-    this.onPressed, {
-    this.text1 = 'Ainda não possui uma conta?',
-    this.text2 = 'Cadastre-se',
-    super.key,
-  });
-
+  // Texto padrão à esquerda do texto clicável.
   final String text1;
+
+  // Texto clicável.
   final String text2;
+
+  // Função a ser executada ao clicar no texto clicável.
   final VoidCallback onPressed;
+
+  const SocialFooter({
+    Key? key,
+    this.text1 = tDontHaveAnAccount,
+    this.text2 = tSignup,
+    required this.onPressed,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    // Inicialização do controller.
     final controller = Get.put(LoginController());
 
     return Container(
       padding: const EdgeInsets.only(top: 20, bottom: 20),
       child: Column(
         children: [
+          // Botão de login com Google.
           Obx(
             () => SocialButton(
+              // Logo do Google.
               image: googleLogo,
+              // Cor de fundo do botão do Google.
               background: tGoogleBgColor,
+              // Cor do texto do botão do Google.
               foreground: tGoogleForegroundColor,
+              // Texto do botão do Google.
               text: '${tConnectWith.tr} ${tGoogle.tr}',
+              // Se verdadeiro, mostra um indicador de carregamento.
               isLoading: controller.isGoogleLoading.value,
-              onPressed: controller.isLoading.value ? () {} : controller.isGoogleLoading.value ? (){} : () => controller.googleSignIn(),
+              // Define a ação do botão quando pressionado.
+              onPressed:
+                  // Verifica se o login do Facebook ou qualquer outra operação de carregamento está em andamento.
+                  controller.isLoading.value ?
+                      // Se o login do Facebook ou outra operação estiver carregando, desativa a ação do botão.
+                      () {} :
+                      // Caso contrário, verifica se o login do Google está em andamento.
+                      controller.isGoogleLoading.value ?
+                          // Se o login do Google estiver carregando, desativa a ação do botão.
+                          () {} :
+                          // Se nenhum carregamento estiver em andamento, inicia o processo de login do Google.
+                          () => controller.googleSignIn(),
             ),
           ),
           const Gap(20),
