c4b71fa31dc3b9a1a2849e6ff1bfeee3967d5130
Barba2k2 <lorenzo.dz@hotmail.com>
2023-11-13 16:03:45 -0300

    added the new images on image strings

--- lib/src/constants/image_strings.dart
+++ lib/src/constants/image_strings.dart
@@ -1,1 +1,4 @@
 const String googleLogo = 'assets/images/google.png';
+const String moneyLogoPng = 'assets/images/logo.png'; //@ 1000x1000
+const String moneyLogoSvg = 'assets/images/logo.svg'; //# 250x250
+const String tForgetPasswordImage = "assets/images/forget-password.png";
