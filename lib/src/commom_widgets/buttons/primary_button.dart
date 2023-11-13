7d8b0b0dce7c3d1d778deacf1bc93832e3b89d03
Barba2k2 <lorenzo.dz@hotmail.com>
2023-11-13 16:03:26 -0300

    adjusts on primary button

--- lib/src/commom_widgets/buttons/primary_button.dart
+++ lib/src/commom_widgets/buttons/primary_button.dart
@@ -1,28 +1,40 @@
 import 'package:flutter/material.dart';
-import 'package:racha_conta/src/commom_widgets/buttons/button_loading_widgte.dart';
 
+import 'button_loading_widgte.dart';
+
+// Widget personalizado para um botão primário.
 class MyPrimaryButton extends StatelessWidget {
-  const MyPrimaryButton(
-    this.text,
-    this.onPressed, {
+  // Construtor que aceita parâmetros para personalizar o botão.
+  const MyPrimaryButton({
+    Key? key,
+    required this.text,
+    required this.onPressed,
     this.isLoading = false,
-    this.isFullWidth = false,
-    this.width = 100,
-    super.key,
-  });
+    this.isFullWidth = true,
+    this.width = 100.0,
+  }) : super(key: key);
 
+  // Texto que será exibido no botão.
   final String text;
+  // Ação a ser executada quando o botão for pressionado.
   final VoidCallback onPressed;
+  // Indica se o botão está em estado de carregamento.
   final bool isLoading;
+  // Determina se o botão deve ocupar a largura total disponível.
   final bool isFullWidth;
+  // Define a largura do botão quando `isFullWidth` é `false`.
   final double width;
 
+  // Constrói a interface visual do botão.
   @override
   Widget build(BuildContext context) {
     return SizedBox(
+      // Define a largura com base na propriedade `isFullWidth`.
       width: isFullWidth ? double.infinity : width,
       child: ElevatedButton(
         onPressed: onPressed,
+        // Exibe um widget de carregamento se `isLoading` for verdadeiro,
+        // caso contrário, exibe o texto fornecido.
         child: isLoading
             ? const ButtonLoadingWidget()
             : Text(
