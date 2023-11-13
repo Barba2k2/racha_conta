12901845f789fe74e950f19e5056e445bf5665a9
Barba2k2 <lorenzo.dz@hotmail.com>
2023-11-13 16:04:55 -0300

    added new functions on controller

--- lib/src/expenses/controllers/expense_controller.dart
+++ lib/src/expenses/controllers/expense_controller.dart
@@ -1,6 +1,11 @@
+import 'dart:async';
+import 'dart:developer';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:racha_conta/src/utils/helper/helper_controller.dart';
 
 import '../../models/expense_model.dart';
 
@@ -46,8 +51,91 @@ class ExpenseController extends GetxController {
 
       final expenseId = await ExpenseModel.generateExpenseId();
 
+      final expense = ExpenseModel(
+        expenseId: expenseId,
+        userId: user!.uid,
+        title: title,
+        ammount: ammount,
+        date: date,
+        category: category,
+        description: description,
+      );
+
+      DocumentReference documentReference = _firestore
+          .collection('Users')
+          .doc(_auth.currentUser!.uid)
+          .collection('Expenses')
+          .doc(expenseId);
+
+      await documentReference.set(expense.toMap());
+
+      Helper.successSnackBar(
+        title: 'Sucesso',
+        message: 'Seu rolê foi salvo e guardado com segurnaça.',
+      );
     } catch (e) {
-      
+      Helper.errorSnackBar(
+        title: 'Erro',
+        message: 'Houve um erro ao salvar seu rolê $e',
+      );
     }
+  }
+
+  Future<int> getExpenseNumber(String userId) async {
+    try {
+      QuerySnapshot expenseSnapshot = await _firestore
+          .collection('Users')
+          .doc(userId)
+          .collection('Expenses')
+          .get();
+
+      return expenseSnapshot.docs.length + 1;
+    } catch (e) {
+      log('Erro ao obter o número de despesas para o usuário: $e');
+      rethrow;
+    }
+  }
+
+  List getExpenses(AsyncSnapshot snapshot) {
+    try {
+      final expenseList = snapshot.data.docs.map((doc) {
+        final data = doc.data() as Map<String, dynamic>;
+        return ExpenseModel(
+          expenseId: data['Id da Despesa'],
+          userId: data['Id do Usuario'],
+          title: data['Titulo'],
+          ammount: data['Valor'],
+          date: data['Data da Despesa'],
+          category: data['Categoria'],
+          description: data['Descricao'],
+        );
+      }).toList();
+      return expenseList;
+    } catch (e) {
+      log('Erro ao obter as despesas: $e');
+      return [];
+    }
+  }
+
+  // StreamController para lidar com as despesas de um usuário específico.
+  final StreamController<QuerySnapshot> _streamController =
+      StreamController.broadcast();
+
+  Stream<QuerySnapshot> stream() {
+    _firestore
+        .collection('Users')
+        .doc(_auth.currentUser!.uid)
+        .collection('Expenses')
+        .snapshots()
+        .listen(
+      (data) {
+        _streamController.add(data);
+      },
+      onError: (error) {
+        _streamController.addError(error);
+      },
+    );
+
+    return _streamController.stream;
   }
 }
