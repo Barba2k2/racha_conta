f6916b51a384c25c864ae3e843d8280ca6c3bcf1
Barba2k2 <lorenzo.dz@hotmail.com>
2023-11-18 17:19:06 -0300

    added expenseWidget

--- lib/src/expenses/expenses_page/expanses_list/expenses_list.dart
+++ lib/src/expenses/expenses_page/expanses_list/expenses_list.dart
@@ -11,92 +11,107 @@ class ExpenseList extends StatelessWidget {
     super.key,
     required this.expenses,
     required this.onRemoveExpense,
+    required this.expenseWidget,
   });
 
   final List<ExpenseModel> expenses;
   final void Function(ExpenseModel expense) onRemoveExpense;
+  final Widget expenseWidget;
 
   @override
   Widget build(BuildContext context) {
     final ThemeController themeController = Get.find();
     final isDark = themeController.isDarkMode.value;
-    return ListView.builder(
-      itemCount: expenses.length,
-      itemBuilder: (ctx, index) => Dismissible(
-        key: ValueKey(expenses[index]),
-        direction: DismissDirection.endToStart,
-        background: Container(
-          decoration: BoxDecoration(
-            color: isDark ? tDarkCard : tLightCard,
-            borderRadius: BorderRadius.circular(20),
-          ),
-          margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-          alignment: Alignment.centerRight,
-          child: const Padding(
-            padding: EdgeInsets.only(right: 20.0),
-            child: Icon(
-              Icons.delete,
-              color: Colors.white,
-              size: 28,
+
+    return Column(
+      children: [
+        Expanded(
+          child: ListView.builder(
+            itemCount: expenses.length,
+            itemBuilder: (ctx, index) => Dismissible(
+              key: ValueKey(expenses[index]),
+              direction: DismissDirection.endToStart,
+              background: Container(
+                decoration: BoxDecoration(
+                  color: isDark ? tDarkCard : tLightCard,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                margin: const EdgeInsets.symmetric(
+                  horizontal: 10,
+                  vertical: 10,
+                ),
+                alignment: Alignment.centerRight,
+                child: const Padding(
+                  padding: EdgeInsets.only(right: 20.0),
+                  child: Icon(
+                    Icons.delete,
+                    color: Colors.white,
+                    size: 28,
+                  ),
+                ),
+              ),
+              confirmDismiss: (direction) async {
+                return await showDialog<bool>(
+                  context: context,
+                  builder: (BuildContext dialogContext) {
+                    return AlertDialog(
+                      title: Text(
+                        'Confirmação',
+                        style: Theme.of(context).textTheme.headlineMedium,
+                        textAlign: TextAlign.center,
+                      ),
+                      content: Text(
+                        'Tem certeza que deseja apagar esta despesa?',
+                        style: Theme.of(context).textTheme.headlineSmall,
+                      ),
+                      actions: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            OutlinedButton(
+                              onPressed: () =>
+                                  Navigator.of(dialogContext).pop(false),
+                              style: OutlinedButton.styleFrom(
+                                padding: const EdgeInsets.symmetric(
+                                  vertical: 15,
+                                  horizontal: 40,
+                                ),
+                              ),
+                              child: const Text(
+                                'Não',
+                                style: TextStyle(fontSize: 16),
+                              ),
+                            ),
+                            ElevatedButton(
+                              style: OutlinedButton.styleFrom(
+                                padding: const EdgeInsets.symmetric(
+                                  vertical: 15,
+                                  horizontal: 40,
+                                ),
+                              ),
+                              child: const Text(
+                                'Sim',
+                                style: TextStyle(fontSize: 16),
+                              ),
+                              onPressed: () =>
+                                  Navigator.of(dialogContext).pop(true),
+                            ),
+                          ],
+                        ),
+                      ],
+                    );
+                  },
+                );
+              },
+              onDismissed: (direction) {
+                onRemoveExpense(expenses[index]);
+              },
+              child: ExpenseCard(expenses[index]),
             ),
           ),
         ),
-        confirmDismiss: (direction) async {
-          return await showDialog<bool>(
-            context: context,
-            builder: (BuildContext dialogContext) {
-              return AlertDialog(
-                title: Text(
-                  'Confirmação',
-                  style: Theme.of(context).textTheme.headlineMedium,
-                  textAlign: TextAlign.center,
-                ),
-                content: Text(
-                  'Tem certeza que deseja apagar esta despesa?',
-                  style: Theme.of(context).textTheme.headlineSmall,
-                ),
-                actions: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      OutlinedButton(
-                        onPressed: () => Navigator.of(dialogContext).pop(false),
-                        style: OutlinedButton.styleFrom(
-                          padding: const EdgeInsets.symmetric(
-                            vertical: 15,
-                            horizontal: 40,
-                          ),
-                        ),
-                        child: const Text(
-                          'Não',
-                          style: TextStyle(fontSize: 16),
-                        ),
-                      ),
-                      ElevatedButton(
-                        style: OutlinedButton.styleFrom(
-                          padding: const EdgeInsets.symmetric(
-                            vertical: 15,
-                            horizontal: 40,
-                          ),
-                        ),
-                        child: const Text(
-                          'Sim',
-                          style: TextStyle(fontSize: 16),
-                        ),
-                        onPressed: () => Navigator.of(dialogContext).pop(true),
-                      ),
-                    ],
-                  ),
-                ],
-              );
-            },
-          );
-        },
-        onDismissed: (direction) {
-          onRemoveExpense(expenses[index]);
-        },
-        child: ExpenseCard(expenses[index]),
-      ),
+        expenseWidget,
+      ],
     );
   }
 }
