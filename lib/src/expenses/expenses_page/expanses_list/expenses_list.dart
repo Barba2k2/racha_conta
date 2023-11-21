1223f265a108510cb1ba88d4c2dbe6b1c3a26645
Barba2k2 <lorenzo.dz@hotmail.com>
2023-11-20 22:16:02 -0300

    refact: expenses list

--- lib/src/expenses/expenses_page/expanses_list/expenses_list.dart
+++ lib/src/expenses/expenses_page/expanses_list/expenses_list.dart
@@ -1,117 +1,109 @@
+import 'dart:developer';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:racha_conta/src/constants/colors.dart';
 
+import '../../../constants/colors.dart';
 import '../../../controllers/theme_controller/theme_controller.dart';
 import '../../../models/expense_model.dart';
+import '../../controllers/expense_controller.dart';
 import '../widgets/expense_card.dart';
+import '../widgets/expense_detail.dart';
 
-class ExpenseList extends StatelessWidget {
-  const ExpenseList({
-    super.key,
-    required this.expenses,
-    required this.onRemoveExpense,
-    required this.expenseWidget,
-  });
+class ExpensesList extends StatefulWidget {
+  const ExpensesList({Key? key}) : super(key: key);
 
-  final List<ExpenseModel> expenses;
-  final void Function(ExpenseModel expense) onRemoveExpense;
-  final Widget expenseWidget;
+  @override
+  State<ExpensesList> createState() => _ExpensesListState();
+}
+
+class _ExpensesListState extends State<ExpensesList> {
+  late List<ExpenseModel> _registeredExpenses = [];
+
+  @override
+  void initState() {
+    _registeredExpenses = [];
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     final ThemeController themeController = Get.find();
     final isDark = themeController.isDarkMode.value;
 
-    return Column(
-      children: [
-        Expanded(
-          child: ListView.builder(
-            itemCount: expenses.length,
-            itemBuilder: (ctx, index) => Dismissible(
-              key: ValueKey(expenses[index]),
-              direction: DismissDirection.endToStart,
-              background: Container(
-                decoration: BoxDecoration(
-                  color: isDark ? tDarkCard : tLightCard,
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                margin: const EdgeInsets.symmetric(
-                  horizontal: 10,
-                  vertical: 10,
-                ),
-                alignment: Alignment.centerRight,
-                child: const Padding(
-                  padding: EdgeInsets.only(right: 20.0),
-                  child: Icon(
-                    Icons.delete,
-                    color: Colors.white,
-                    size: 28,
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          StreamBuilder(
+            stream: ExpenseController().stream(),
+            builder: (context, snapshot) {
+              if (snapshot.hasError) {
+                log('Erro da snapshot da pagina expenses: ${snapshot.error}');
+              }
+
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(
+                  child: CircularProgressIndicator(
+                    color: Colors.purple,
+                    strokeWidth: 5,
+                    strokeAlign: BorderSide.strokeAlignCenter,
+                    semanticsLabel: 'Carregando rolês...',
                   ),
-                ),
-              ),
-              confirmDismiss: (direction) async {
-                return await showDialog<bool>(
-                  context: context,
-                  builder: (BuildContext dialogContext) {
-                    return AlertDialog(
-                      title: Text(
-                        'Confirmação',
-                        style: Theme.of(context).textTheme.headlineMedium,
-                        textAlign: TextAlign.center,
-                      ),
-                      content: Text(
-                        'Tem certeza que deseja apagar esta despesa?',
-                        style: Theme.of(context).textTheme.headlineSmall,
-                      ),
-                      actions: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            OutlinedButton(
-                              onPressed: () =>
-                                  Navigator.of(dialogContext).pop(false),
-                              style: OutlinedButton.styleFrom(
-                                padding: const EdgeInsets.symmetric(
-                                  vertical: 15,
-                                  horizontal: 40,
-                                ),
-                              ),
-                              child: const Text(
-                                'Não',
-                                style: TextStyle(fontSize: 16),
-                              ),
-                            ),
-                            ElevatedButton(
-                              style: OutlinedButton.styleFrom(
-                                padding: const EdgeInsets.symmetric(
-                                  vertical: 15,
-                                  horizontal: 40,
-                                ),
-                              ),
-                              child: const Text(
-                                'Sim',
-                                style: TextStyle(fontSize: 16),
-                              ),
-                              onPressed: () =>
-                                  Navigator.of(dialogContext).pop(true),
-                            ),
-                          ],
-                        ),
-                      ],
-                    );
-                  },
                 );
-              },
-              onDismissed: (direction) {
-                onRemoveExpense(expenses[index]);
-              },
-              child: ExpenseCard(expenses[index]),
-            ),
+              }
+
+              if (!snapshot.hasData || snapshot.data == null) {
+                return Center(
+                  child: Text(
+                    'Nenhum dado disponível',
+                    style: TextStyle(
+                      color: isDark ? whiteColor : blackColor,
+                    ),
+                  ),
+                );
+              }
+
+              final expenseList = snapshot.data!.docs
+                  .map(
+                    (doc) => ExpenseModel.fromSnapshot(
+                      doc as QueryDocumentSnapshot<Map<String, dynamic>>,
+                    ),
+                  )
+                  .toList();
+
+              return ListView.builder(
+                shrinkWrap: true,
+                itemCount: _registeredExpenses.length + expenseList.length,
+                itemBuilder: (context, index) {
+                  if (index < _registeredExpenses.length) {
+                    final expense = _registeredExpenses[index];
+                    return GestureDetector(
+                      onTap: () {
+                        Get.to(() => ExpenseDetails(expenseModel: expense));
+                        log('Botão detalhes');
+                      },
+                      child: ExpenseCard(expense),
+                    );
+                  } else {
+                    final firestoreIndex = index - _registeredExpenses.length;
+                    final firestoreExpense = expenseList[firestoreIndex];
+                    return GestureDetector(
+                      onTap: () {
+                        Get.to(
+                          () => ExpenseDetails(expenseModel: firestoreExpense),
+                        );
+                        log('Botão detalhes');
+                      },
+                      child: ExpenseCard(firestoreExpense),
+                    );
+                  }
+                },
+              );
+            },
           ),
-        ),
-        expenseWidget,
-      ],
+        ],
+      ),
     );
   }
 }
